be470cda9e500a66cb7976f97c3739add4d9f0fd
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-07-22 12:56:24 -0600

    Restructure database to have per-guild data

--- src/types/database.d.ts
+++ src/types/database.d.ts
@@ -1,12 +1,14 @@
 interface database {
-	webhook: {
-		id: string;
-		token: string;
-	};
-	bracket: {
-		msg: string;
-		quotes: string[];
-		votes: { [index: number]: number };
-		users: { [index: string]: number };
-	};
+	[index: string]: {
+		webhook: {
+			id: string;
+			token: string;
+		};
+		bracket: {
+			msg: string;
+			quotes: string[];
+			votes: { [index: number]: number };
+			users: { [index: string]: number };
+		};
+	}
 }
