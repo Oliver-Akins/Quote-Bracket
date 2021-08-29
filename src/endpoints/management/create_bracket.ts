f1586efc9e3ae1d05d5b32990d405d15323b166c
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-08-29 15:14:05 -0600

    Move location of colon in field name

--- src/endpoints/management/create_bracket.ts
+++ src/endpoints/management/create_bracket.ts
@@ -64,7 +64,7 @@ export default {
 				{
 					description: `Note: If **more than ${Math.floor(config.guilds[gID].quote_max / 2)}** of the quotes tie, they will all be eliminated, otherwise, the ones that tie will move on to the next bracket.`,
 					fields: quotes.map((quote, i) => { return {
-						name: `${i < winner_count ? '👑 ' : ''}Quote: ${i + 1}`,
+						name: `${i < winner_count ? '👑 ' : ''}Quote ${i + 1}:`,
 						value: quote,
 					}}),
 				}
